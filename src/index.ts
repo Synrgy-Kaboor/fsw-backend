710e462aae7c1ab1a815191f2054ddf03fc9221f
Ignasius Ferry <ignasius.ferry01@gmail.com>
2024-02-02 18:54:12 +0700

    fix: jwt token always invalid error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { App } from './App';
 import { type Routes } from '@routes/Routes';
 import UserRoutes from '@routes/UserRoutes';
+import BookingRoutes from '@routes/BookingRoutes';
 
 const routes: Routes[] = [
-  new UserRoutes()
+  new UserRoutes(),
+  new BookingRoutes()
 ];
 
 const app = new App(routes);
