4011a7de0436bae6455f6a1ab464033b8704c168
Ignasius Ferry <ignasius.ferry01@gmail.com>
2024-02-03 14:03:41 +0700

    fix: remove cities table from db

--- src/database/seeds/005_airports.ts
+++ src/database/seeds/005_airports.ts
@@ -7,25 +7,21 @@ export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
   await knex('airports').insert([
     {
-      city_id: 1,
       code: 'SUB',
       name: 'Juanda International Airport',
       timezone: 7
     },
     {
-      city_id: 2,
       code: 'CGK',
       name: 'Soekarno-Hatta Interational Airport',
       timezone: 7
     },
     {
-      city_id: 2,
       code: 'HLP',
       name: 'Halim Perdanakusuma International Airport',
       timezone: 7
     },
     {
-      city_id: 3,
       code: 'PDG',
       name: 'Minangkabau International Airport',
       timezone: 8
