0cb5f268ab23ea4993bee18fffd0d027a7b25281
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-02-08 16:29:19 +0700

    fix: lint generate otp

--- src/utils/generateOtp.ts
+++ src/utils/generateOtp.ts
@@ -1,4 +1,4 @@
-const generateOtp = (length: number) => {
+const generateOtp = (length: number): string => {
   let result = '';
   const characters = '0123456789';
   for (let i = 0; i < length; i++) {
