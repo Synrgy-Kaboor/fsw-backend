4410c2a7c8eef1163ed50778424fa997e8d91c70
Ignasius Ferry <ignasius.ferry01@gmail.com>
2024-02-11 17:41:07 +0700

    fix: dateUtils not using UTC

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -3,9 +3,9 @@ export function stringToDate(s: string): Date {
 }
 
 export function dateToString(d: Date): string {
-  const year = '' + d.getFullYear();
-  let month = '' + (d.getMonth() + 1);
-  let date = '' + d.getDate();
+  const year = '' + d.getUTCFullYear();
+  let month = '' + (d.getUTCMonth() + 1);
+  let date = '' + d.getUTCDate();
 
   if (month.length < 2) month = '0' + month;
   if (date.length < 2) date = '0' + date;
@@ -22,7 +22,7 @@ export function addHours(d: Date, hours: number): Date {
 
 export function addMinutes(d: Date, minutes: number): Date {
   const newDate = new Date(d);
-  newDate.setMinutes(d.getMinutes() + minutes);
+  newDate.setMinutes(d.getUTCMinutes() + minutes);
   
   return newDate;
 }
@@ -36,7 +36,7 @@ export function timezoneString(timezone: number): string {
 export function timeWithTimezone(d: Date, timezone: number): string {
   const newDate = addHours(d, timezone);
   const hours = newDate.getUTCHours();
-  const minutes = newDate.getMinutes();
+  const minutes = newDate.getUTCMinutes();
 
   const hoursString = hours < 10 ? `0${hours}` : `${hours}`;
   const minutesString = minutes < 10 ? `0${minutes}` : `${minutes}`;
@@ -53,17 +53,17 @@ export function dateToVerboseString(d: Date, timezone: number = 0): string {
 
   const date = addHours(d, timezone);
 
-  return `${dayArr[date.getDay()]}, ${date.getDate()} ${monthArr[date.getMonth()]} ${date.getFullYear()}`;
+  return `${dayArr[date.getUTCDay()]}, ${date.getUTCDate()} ${monthArr[date.getUTCMonth()]} ${date.getUTCFullYear()}`;
 }
 
 export function durationString(start: Date, end: Date): string {
-  const startDate = start.getDate();
-  const endDate = end.getDate();
+  const startDate = start.getUTCDate();
+  const endDate = end.getUTCDate();
 
-  let hours = end.getHours() - start.getHours();
+  let hours = end.getUTCHours() - start.getUTCHours();
   if (endDate > startDate) hours += 24;
 
-  let minutes = end.getMinutes() - start.getMinutes();
+  let minutes = end.getUTCMinutes() - start.getUTCMinutes();
   if (minutes < 0) {
     hours -= 1;
     minutes = 60 - minutes;
