0d29cb530f6d38ed65024a228073b8a8ce2f84ba
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-02-13 00:26:46 +0700

    feat: register verify otp, resend otp

--- src/repositories/RegisterRepository.ts
+++ src/repositories/RegisterRepository.ts
@@ -1,12 +1,39 @@
-import { type User,  UserModel } from "@models/UserModel";
+import { type User, UserModel } from '@models/UserModel';
+import { getNextFiveMinutesOnSeconds } from '@utils/dateUtils';
+import { generateOtp } from '@utils/generateOtp';
 
 export class RegisterRepository {
-
-    public async register(user: Partial<User>): Promise<User> {
-        const findUser = await UserModel.query().where({ email: user.email }).first();
-        if (findUser) {
-            throw new Error("User already exists");
-        }
-        return await UserModel.query().insert(user);
+  public async register(user: Partial<User>): Promise<User> {
+    const findUser = await UserModel.query()
+      .where({ email: user.email })
+      .first();
+    if (findUser) {
+      throw new Error('User already exists');
     }
+    return await UserModel.query().insert(user);
+  }
+
+  public async verifyOtp(otp: string): Promise<User> {
+    return await UserModel.query()
+      .patch({
+        otp: undefined,
+        verified: true,
+      })
+      .where({ otp })
+      .returning('*')
+      .first()
+      .throwIfNotFound();
+  }
+
+  public async resendOtp(email: string): Promise<User> {
+    return await UserModel.query()
+      .patch({
+        otp: generateOtp(4),
+        verify_deadlines: getNextFiveMinutesOnSeconds(),
+      })
+      .where({ email })
+      .returning('*')
+      .first()
+      .throwIfNotFound();
+  }
 }
--- src/services/RegisterService.ts
+++ src/services/RegisterService.ts
@@ -3,19 +3,16 @@ import { RegisterRepository } from '@repositories/RegisterRepository';
 import { generateOtp } from '@utils/generateOtp';
 import { SendMailJet } from '@utils/sendMail';
 import bcrypt from 'bcrypt';
-const getNextFiveMinutesOnSeconds = (): number => {
-  const currentSeconds = getCurrentSeconds();
-  return currentSeconds + 300;
-};
-
-const getCurrentSeconds = (): number => {
-  const now = new Date();
-  return Math.floor(now.getTime() / 1000); // Convert milliseconds to seconds
-};
+import {
+  getCurrentSeconds,
+  getNextFiveMinutesOnSeconds,
+} from '@utils/dateUtils';
+import { UserRepository } from '@repositories/UserRepository';
 
 export class RegisterService {
   private readonly saltRounds = 10;
   private readonly registerRepository = new RegisterRepository();
+  private readonly userRepository = new UserRepository();
   public async register(user: Partial<User>): Promise<User> {
     user.password = user.password
       ? bcrypt.hashSync(user.password, this.saltRounds)
@@ -34,5 +31,27 @@ export class RegisterService {
       `Your OTP for verification: ${user.otp}`,
     );
     return newUser;
+  }
+
+  public async verifyOtpRegister(otp: string): Promise<User> {
+    const findUser = await this.userRepository.getUserByOtp(otp);
+    if (findUser.verify_deadlines < getCurrentSeconds()) {
+      throw new Error('OTP Expired');
+    }
+    return await this.registerRepository.verifyOtp(otp);
+  }
+
+  public async resendOtpRegister(email: string): Promise<User> {
+    const findUser = await this.registerRepository.resendOtp(email);
+    await SendMailJet(
+      [
+        {
+          Email: findUser.email,
+          Name: findUser.full_name,
+        },
+      ],
+      `Your OTP for verification: ${findUser.otp}`,
+    );
+    return findUser;
   }
 }
