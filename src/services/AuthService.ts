6e58e242d6953f29aa1b7aef33a88d27450cff5c
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2024-02-13 01:48:31 +0700

    fix: lint

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -33,31 +33,24 @@ export class AuthService {
   public async verifyForgetPasswordOtp(
     email: string,
     otp: string,
-  ): Promise<User | void> {
-    try {
-      const findUser = await this.userRepository.getUserByEmail(email);
-      if (findUser.request_for_change_password_otp !== otp) {
-        throw new Error('Invalid OTP');
-      } else {
-        if (findUser.forget_password_verify_deadlines < getCurrentSeconds()) {
-          throw new Error('OTP Expired');
-        }
-        await this.userRepository.updatePersonalInformationByEmail(email, {
-          request_for_change_password_verified: true,
-        });
-        return findUser;
+  ): Promise<User> {
+    const findUser = await this.userRepository.getUserByEmail(email);
+    if (findUser.request_for_change_password_otp !== otp) {
+      throw new Error('Invalid OTP');
+    } else {
+      if (findUser.forget_password_verify_deadlines < getCurrentSeconds()) {
+        throw new Error('OTP Expired');
       }
-    } catch (e) {
-      throw e;
+      await this.userRepository.updatePersonalInformationByEmail(email, {
+        request_for_change_password_verified: true,
+      });
+      return findUser;
     }
   }
 
-  public async updatePassword(
-    email: string,
-    password: string,
-  ): Promise<User | void> {
+  public async updatePassword(email: string, password: string): Promise<User> {
     const findUser = await this.userRepository.getUserByEmail(email);
-    if (findUser.request_for_change_password_verified === false) {
+    if (!findUser.request_for_change_password_verified) {
       throw new Error('OTP Not Verified');
     } else {
       const hashPassword = bcrypt.hashSync(password, this.saltRounds);
